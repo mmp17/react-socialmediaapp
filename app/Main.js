8d5c2f4d68f332ec77fb79b48f53798abd178b34
mmp17 <mirzaevmuzaffar@yahoo.com>
2022-08-20 13:00:06 +0900

    deploy

--- app/Main.js
+++ app/Main.js
@@ -4,7 +4,7 @@ import { useImmerReducer } from "use-immer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { CSSTransition } from "react-transition-group"
 import Axios from "axios"
-Axios.defaults.baseURL = process.env.BACKENDURL || ""
+Axios.defaults.baseURL = process.env.BACKENDURL || "https://mfrsocialmediareactbackend.herokuapp.com"
 
 import StateContext from "./StateContext"
 import DispatchContext from "./DispatchContext"
--- app/components/Chat.js
+++ app/components/Chat.js
@@ -24,7 +24,7 @@ function Chat() {
   }, [appState.isChatOpen])
 
   useEffect(() => {
-    socket.current = io("http://localhost:8080")
+    socket.current = io(process.env.BACKENDURL || "https://mfrsocialmediareactbackend.herokuapp.com")
 
     socket.current.on("chatFromServer", (message) => {
       setState((draft) => {
